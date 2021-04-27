607e44d6ada103a6531166d96e0f8aca74f8ea90
byulutas <ulutasbayram7@gmail.com>
2021-04-27 12:34:49 +0300

    Arayüz ile veritabanı bağlantısını sağlayan fonksiyonlar yapıldı.

--- E_Ticaret_Sitesi/src/main/java/com/mycompany/e_ticaret_sitesi/dao/KategoriDao.java
+++ E_Ticaret_Sitesi/src/main/java/com/mycompany/e_ticaret_sitesi/dao/KategoriDao.java
@@ -1,8 +1,49 @@
 package com.mycompany.e_ticaret_sitesi.dao;
 
-
+import com.mycompany.e_ticaret_sitesi.entities.Kategori;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class KategoriDao {
 
+    private SessionFactory factory;
+
+    public KategoriDao(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    //kategori veritabanına kaydeder
+    public int saveCategory(Kategori kat) {
+        Session session = this.factory.openSession();
+        Transaction tx = session.beginTransaction();
+        int katId = (int) session.save(kat);
+        tx.commit();
+
+        session.close();
+        return katId;
+    }
+
+    public List<Kategori> getCategories() {
+        Session s = this.factory.openSession();
+        Query query = s.createQuery("from Kategori");
+        List<Kategori> list = query.list();
+        return list;
+    }
+
+    public Kategori getKategoriById(int kid) {
+        Kategori kat = null;
+        try {
+            Session session = this.factory.openSession();
+            kat = session.get(Kategori.class, kid);
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return kat;
+    }
 
 }
